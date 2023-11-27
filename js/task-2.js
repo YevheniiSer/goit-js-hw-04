1855bd07f422f16839a79c2bf5e56a09ef177fa4
yevheniiser <141223975+YevheniiSer@users.noreply.github.com>
2023-11-27 02:08:06 +0200

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,13 @@
+"use strict"
+function makeArray(firstArray, secondArray, maxLength) {
+    let fullArray = firstArray.concat(secondArray);
+    const cutArray = fullArray.length > maxLength ? fullArray.slice(0, maxLength) : fullArray;
+    return cutArray
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
